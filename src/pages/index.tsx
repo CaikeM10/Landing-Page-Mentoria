2705c00c7f6b66c52178aa27bfc5973ec8cff41c
Leonardo Dorea <leolimadorea@gmail.com>
2024-03-25 08:49:33 -0300

    feat: section three and scroll bars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import Guarantee from "@/components/Guarantee";
 import Header from "@/components/Header";
+import MovingBars from "@/components/MovingBars";
 import SectionOne from "@/components/SectionOne";
+import SectionTwo from "@/components/SectionTwo";
 
 export default function Home() {
   return (
     <>
       <Header />
       <SectionOne />
+      <SectionTwo />
+      <MovingBars />
+      <Guarantee />
     </>
   );
 }
