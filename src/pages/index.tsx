ec63805f9ee2e33c2f6242a6463e8925c2d21dd0
Leonardo Dorea <leolimadorea@gmail.com>
2024-03-27 03:23:07 -0300

    feat: horizontal, bonus, price, about, footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
+import About from "@/components/About";
+import Bonus from "@/components/Bonus";
+import Footer from "@/components/Footer";
 import Guarantee from "@/components/Guarantee";
 import Header from "@/components/Header";
 import MovingBars from "@/components/MovingBars";
-import HorizontalScrollPage from "@/components/NewTest";
+import Price from "@/components/Price";
 import SectionOne from "@/components/SectionOne";
 import SectionTwo from "@/components/SectionTwo";
+import HorizontalScrollPage from "@/components/SideSlide";
 import Students from "@/components/Swiper";
 import Who from "@/components/Who";
+import { useState } from "react";
 
 export default function Home() {
+  const [hasScrolledToEnd, setHasScrolledToEnd] = useState(false);
+
+  const handleScrollToEnd = () => {
+    setHasScrolledToEnd(true);
+  };
+
   return (
     <>
       <Header />
@@ -17,8 +28,15 @@ export default function Home() {
       <Guarantee />
       <Students />
       <Who />
-
-      <HorizontalScrollPage />
+      <HorizontalScrollPage onScrollEnd={handleScrollToEnd} />
+      {hasScrolledToEnd && (
+        <>
+          <Bonus />
+          <Price />
+          <About />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
