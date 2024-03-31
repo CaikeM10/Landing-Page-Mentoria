802db22cbd05169ed65bfafba5696e574e0b4d0a
Leonardo Dorea <leolimadorea@gmail.com>
2024-03-31 00:59:13 -0300

    feat: responsive and better horizontal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import Bonus from "@/components/Bonus";
 import Footer from "@/components/Footer";
 import Guarantee from "@/components/Guarantee";
 import Header from "@/components/Header";
+import HybridScroll from "@/components/HybridScoll";
 import MovingBars from "@/components/MovingBars";
 import Price from "@/components/Price";
 import SectionOne from "@/components/SectionOne";
 import SectionTwo from "@/components/SectionTwo";
-import HorizontalScrollPage from "@/components/SideSlide";
 import Students from "@/components/Swiper";
 import Who from "@/components/Who";
 import { useState } from "react";
@@ -19,12 +19,12 @@ export default function Home() {
 
   const handleScrollToEnd = () => {
     setHasScrolledToEnd(true);
-    setCanScrollUpPastHorizontalPage(false); // Prevent scrolling up past HorizontalScrollPage
+    setCanScrollUpPastHorizontalPage(false);
   };
 
   const handleScrollToStart = () => {
     setHasScrolledToEnd(false);
-    setCanScrollUpPastHorizontalPage(true); // Allow scrolling up past HorizontalScrollPage
+    setCanScrollUpPastHorizontalPage(true);
   };
 
   return (
@@ -36,19 +36,13 @@ export default function Home() {
       <Guarantee />
       <Students />
       <Who />
-
-      <HorizontalScrollPage
-        onScrollEnd={handleScrollToEnd}
-        onScrollStart={handleScrollToStart}
-      />
-      {hasScrolledToEnd && (
-        <>
-          <Bonus />
-          <Price />
-          <About />
-          <Footer />
-        </>
-      )}
+      <HybridScroll />
+      <Bonus />
+      <Price />
+      <About />
+      <Footer />
+      {/* <GlowingButton />
+      <Aurora /> */}
     </>
   );
 }
