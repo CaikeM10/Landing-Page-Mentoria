ccf9465806870223e2d69cada2fa27d12095d60f
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-01 17:18:25 -0300

    effects 3

--- src/components/Guarantee/index.tsx
+++ src/components/Guarantee/index.tsx
@@ -1,68 +1,102 @@
-import styles from "./style.module.scss";
+import ScrollAnimation from 'react-animate-on-scroll';
+import styles from './style.module.scss';
 
 const Guarantee = () => {
-  return (
-    <main className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.top}>
-          <div className={styles.leftside}>
-            <div className={styles.leftsideContent}>
-              <h5>Dias de</h5>
-              <h5>garantia</h5>
-              <div className={styles.stars}>
-                <img src="/star.svg" />
-                <img src="/star.svg" />
-                <img src="/star.svg" />
-                <img src="/star.svg" />
-                <img src="/star.svg" />
-              </div>
+    return (
+        <main className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.top}>
+                    <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+                        <div className={styles.leftside}>
+                            <div className={styles.leftsideContent}>
+                                <h5>Dias de</h5>
+                                <h5>garantia</h5>
+                                <div className={styles.stars}>
+                                    <img src="/star.svg" />
+                                    <img src="/star.svg" />
+                                    <img src="/star.svg" />
+                                    <img src="/star.svg" />
+                                    <img src="/star.svg" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.rightside}>
+                            <h5>
+                                Experimente por{' '}
+                                <span className={styles.auroraSpan}>
+                                    7 dias
+                                </span>
+                            </h5>
+                            <p>
+                                Garantindo a sua vaga agora, você não corre
+                                risco nenhum!
+                            </p>
+                            <p>
+                                Você pode ver todo o Protocolo por dentro e,
+                                caso perceba que não faz sentido para o seu
+                                momento, basta me enviar um e-mail e eu vou
+                                devolver 100% do seu investimento.
+                            </p>
+                            <p>O risco é todo meu.</p>
+                        </div>
+                    </ScrollAnimation>
+                </div>
+                <div className={styles.bottom}>
+                    <div className={styles.info}>
+                        <ScrollAnimation
+                            animateIn="fadeInUp"
+                            animateOut="fadeOut"
+                        >
+                            <div className={styles.icon}>
+                                <img src="/safePayment.svg" />
+                            </div>
+                            <div className={styles.text}>
+                                <h5>Pagamento seguro</h5>
+                                <p>
+                                    Diversas formas de pagar, todas com selo de
+                                    segurança.
+                                </p>
+                            </div>
+                        </ScrollAnimation>
+                    </div>
+                    <div className={styles.info}>
+                        <ScrollAnimation
+                            animateIn="fadeInUp"
+                            animateOut="fadeOut"
+                        >
+                            <div className={styles.icon}>
+                                <img src="/lock.svg" />
+                            </div>
+                            <div className={styles.text}>
+                                <h5>Liberação imediata</h5>
+                                <p>
+                                    Receba seu login e senha na mesma hora que
+                                    comprar.
+                                </p>
+                            </div>
+                        </ScrollAnimation>
+                    </div>
+                    <div className={styles.info}>
+                        <ScrollAnimation
+                            animateIn="fadeInUp"
+                            animateOut="fadeOut"
+                        >
+                            <div className={styles.icon}>
+                                <img src="infinite.svg" />
+                            </div>
+                            <div className={styles.text}>
+                                <h5>Acesso vitalício</h5>
+                                <p>
+                                    Compre e assista quando quiser. Não tem
+                                    renovação.
+                                </p>
+                            </div>
+                        </ScrollAnimation>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className={styles.rightside}>
-            <h5>
-              Experimente por <span className={styles.auroraSpan}>7 dias</span>
-            </h5>
-            <p>Garantindo a sua vaga agora, você não corre risco nenhum!</p>
-            <p>
-              Você pode ver todo o Protocolo por dentro e, caso perceba que não
-              faz sentido para o seu momento, basta me enviar um e-mail e eu vou
-              devolver 100% do seu investimento.
-            </p>
-            <p>O risco é todo meu.</p>
-          </div>
-        </div>
-        <div className={styles.bottom}>
-          <div className={styles.info}>
-            <div className={styles.icon}>
-              <img src="/safePayment.svg" />
-            </div>
-            <div className={styles.text}>
-              <h5>Pagamento seguro</h5>
-              <p>Diversas formas de pagar, todas com selo de segurança.</p>
-            </div>
-          </div>
-          <div className={styles.info}>
-            <div className={styles.icon}>
-              <img src="/lock.svg" />
-            </div>
-            <div className={styles.text}>
-              <h5>Liberação imediata</h5>
-              <p>Receba seu login e senha na mesma hora que comprar.</p>
-            </div>
-          </div>
-          <div className={styles.info}>
-            <div className={styles.icon}>
-              <img src="infinite.svg" />
-            </div>
-            <div className={styles.text}>
-              <h5>Acesso vitalício</h5>
-              <p>Compre e assista quando quiser. Não tem renovação.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 };
 
 export default Guarantee;
