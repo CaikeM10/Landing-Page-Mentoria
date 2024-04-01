be7d8591bbf3159c6648f637c41163f68089f9a0
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-01 16:54:34 -0300

    test animation

--- src/components/SectionTwo/index.tsx
+++ src/components/SectionTwo/index.tsx
@@ -1,3 +1,4 @@
+import ScrollAnimation from 'react-animate-on-scroll';
 import styles from './style.module.scss';
 
 const SectionTwo = () => {
@@ -6,77 +7,92 @@ const SectionTwo = () => {
             <main className={styles.container}>
                 <div className={styles.content}>
                     <div className={styles.top}>
-                        <h2>
-                            A diferença absurda entre o que você está{' '}
-                            <span>prestes a </span>
-                            <span>adiquirir com o Rei</span> e do que está
-                            fugindo ...
-                        </h2>
+                        <ScrollAnimation
+                            animateIn="fadeIn"
+                            animateOut="fadeOut"
+                        >
+                            <h2>
+                                A diferença absurda entre o que você está{' '}
+                                <span>prestes a </span>
+                                <span>adiquirir com o Rei</span> e do que está
+                                fugindo ...
+                            </h2>
+                        </ScrollAnimation>
                     </div>
                     <div className={styles.bottom}>
-                        <div className={styles.leftbox}>
-                            <div className={styles.title}>
-                                <h5>Curso meio boca</h5>
+                        <ScrollAnimation
+                            animateIn="bounceInLeft"
+                            animateOut="bounceOutRight"
+                        >
+                            <div className={styles.leftbox}>
+                                <div className={styles.title}>
+                                    <h5>Curso meio boca</h5>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Faturamento incerto</p>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Professor que não vive o que ensina</p>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Falta de organização</p>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Curso básico</p>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Não ensina sobre o mercado real</p>
+                                </div>
+                                <div className={styles.cons}>
+                                    <img src="/redX.svg" />
+                                    <p>Sem suporte</p>
+                                </div>
                             </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Faturamento incerto</p>
+                        </ScrollAnimation>
+                        <ScrollAnimation
+                            animateIn="bounceInRight"
+                            animateOut="bounceOutLeft"
+                        >
+                            <div className={styles.rightbox}>
+                                <div className={styles.title}>
+                                    <h5>
+                                        Curso do <span>REI</span>
+                                    </h5>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Faturamento alto</p>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Professor vive o que ensina</p>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Curso ponta a ponta</p>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Bônus sobre mercado de trabalho</p>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Dicas exclusivas</p>
+                                </div>
+                                <div className={styles.pros}>
+                                    <img src="correct.svg" />
+                                    <p>Suporte exclusivo do rei</p>
+                                </div>
+                                <div className={styles.logo}>
+                                    <img src="/logo.svg" />
+                                </div>
                             </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Professor que não vive o que ensina</p>
-                            </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Falta de organização</p>
-                            </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Curso básico</p>
-                            </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Não ensina sobre o mercado real</p>
-                            </div>
-                            <div className={styles.cons}>
-                                <img src="/redX.svg" />
-                                <p>Sem suporte</p>
-                            </div>
-                        </div>
-                        <div className={styles.rightbox}>
-                            <div className={styles.title}>
-                                <h5>
-                                    Curso do <span>REI</span>
-                                </h5>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Faturamento alto</p>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Professor vive o que ensina</p>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Curso ponta a ponta</p>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Bônus sobre mercado de trabalho</p>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Dicas exclusivas</p>
-                            </div>
-                            <div className={styles.pros}>
-                                <img src="correct.svg" />
-                                <p>Suporte exclusivo do rei</p>
-                            </div>
-                            <div className={styles.logo}>
-                                <img src="/logo.svg" />
-                            </div>
-                        </div>
+                        </ScrollAnimation>
                     </div>
                     <div className={styles.button}>
                         <button>
