88b3adf0ea3b359da0deb57093e7b8a2506f571d
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-01 16:59:27 -0300

    test animation 2

--- src/components/SectionOne/index.tsx
+++ src/components/SectionOne/index.tsx
@@ -6,29 +6,32 @@ const SectionOne = () => {
         <>
             <main className={styles.container}>
                 <div className={styles.content}>
-                    <div className={styles.leftside}>
-                        <img className={styles.logo} src="/logo.svg" />
-                        <h1>
-                            Tudo o que você precisa saber para mudar de vida e{' '}
-                            <span className={styles.auroraSpan}>
-                                ganhar mais de 5 mil reais por semana
-                            </span>{' '}
-                            criando sites da sua casa.
-                        </h1>
-                        <h5>
-                            Aprenda com quem vive o que fala, a fazer da sua
-                            casa o que vai mudar a sua vida! Você já pode
-                            começar hoje fazendo sites de casa e ganhar mais de
-                            R$10mil por mês apartir dos próximos 30 dias.
-                        </h5>
-                        <button>
-                            <p>QUERO MUDAR DE VIDA</p>
-                        </button>
-                        <div className={styles.discount}>
-                            <img src="/discountTag.svg" />
-                            <p>Apenas R$ 16,99.</p>
+                    <ScrollAnimation animateIn="fadeIn">
+                        <div className={styles.leftside}>
+                            <img className={styles.logo} src="/logo.svg" />
+                            <h1>
+                                Tudo o que você precisa saber para mudar de vida
+                                e{' '}
+                                <span className={styles.auroraSpan}>
+                                    ganhar mais de 5 mil reais por semana
+                                </span>{' '}
+                                criando sites da sua casa.
+                            </h1>
+                            <h5>
+                                Aprenda com quem vive o que fala, a fazer da sua
+                                casa o que vai mudar a sua vida! Você já pode
+                                começar hoje fazendo sites de casa e ganhar mais
+                                de R$10mil por mês apartir dos próximos 30 dias.
+                            </h5>
+                            <button>
+                                <p>QUERO MUDAR DE VIDA</p>
+                            </button>
+                            <div className={styles.discount}>
+                                <img src="/discountTag.svg" />
+                                <p>Apenas R$ 16,99.</p>
+                            </div>
                         </div>
-                    </div>
+                    </ScrollAnimation>
                     <ScrollAnimation animateIn="fadeIn">
                         <div className={styles.rightside}>
                             <div className={styles.backgroundImages}>
