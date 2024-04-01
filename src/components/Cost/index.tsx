986942bbd6600c820d18038747c01aa2f62e740c
Leonardo Dorea <leolimadorea@gmail.com>
2024-04-01 00:43:28 -0300

    responsive and hover effect

--- src/components/Cost/index.tsx
+++ src/components/Cost/index.tsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 const Cost = () => {
+  const [lightPosition, setLightPosition] = useState({
+    x: 0,
+    y: 0,
+    visible: false,
+  });
+  const scaleFactor = 1.05;
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    const overlaySize = 100; // Half the overlay size for centering
+    const { left, top } = event.currentTarget.getBoundingClientRect();
+    const adjustedX = (event.clientX - left) / scaleFactor;
+    const adjustedY = (event.clientY - top) / scaleFactor;
+
+    setLightPosition({
+      x: adjustedX - overlaySize,
+      y: adjustedY - overlaySize,
+      visible: true,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setLightPosition((prevPosition) => ({ ...prevPosition, visible: false }));
+  };
+
   return (
     <>
-      <section className={styles.container}>
+      <section
+        className={styles.container}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div
+          className={styles.lightEffect}
+          style={{
+            background: `radial-gradient(circle closest-side, rgba(255, 255, 255, 0.3), transparent)`,
+            transform: `translate(${lightPosition.x}px, ${lightPosition.y}px)`,
+            opacity: lightPosition.visible ? 1 : 0,
+          }}
+        />
         <div className={styles.content}>
           <div className={styles.icon}>
             <img src="/thinArrow.svg" />
