201f663e597e4d274ad86a2935987b5507119c12
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-01 00:08:48 -0300

    landing page

--- src/components/SectionOne/index.tsx
+++ src/components/SectionOne/index.tsx
@@ -1,50 +1,50 @@
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 const SectionOne = () => {
-  return (
-    <>
-      <main className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.leftside}>
-            <img className={styles.logo} src="/logo.svg" />
-            <h1>
-              Tudo o que você precisa saber para mudar de vida e
-              <span className={styles.auroraSpan}>
-                ganhar mais de 5 mil reais por semana
-              </span>{" "}
-              criando sites da sua casa.
-            </h1>
-            <h5>
-              Aprenda com quem vive o que fala, a fazer da sua casa o que vai
-              mudar a sua vida! Você já pode começar hoje fazendo sites de casa
-              e ganhar R$1.200, R$3500 e até mais de R$10mil por mês apartir dos
-              próximos 30 dias
-            </h5>
-            <button>
-              <p>QUERO MUDAR DE VIDA</p>
-            </button>
-            <div className={styles.discount}>
-              <img src="/discountTag.svg" />
-              <p>Apenas 12x R$ 41,33.</p>
-            </div>
-          </div>
-          <div className={styles.rightside}>
-            <div className={styles.backgroundImages}>
-              <img src="/pix1.svg" alt="Background 1" />
-              <img src="/pix2.svg" alt="Background 2" />
-              <img src="/pix3.svg" alt="Background 3" />
-              <img src="/pix4.svg" alt="Background 4" />
-            </div>
-            <img
-              src="/herbertOnly.svg"
-              alt="Herbert"
-              className={styles.herbert}
-            />
-          </div>
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.leftside}>
+                        <img className={styles.logo} src="/logo.svg" />
+                        <h1>
+                            Tudo o que você precisa saber para mudar de vida e{' '}
+                            <span className={styles.auroraSpan}>
+                                ganhar mais de 5 mil reais por semana
+                            </span>{' '}
+                            criando sites da sua casa.
+                        </h1>
+                        <h5>
+                            Aprenda com quem vive o que fala, a fazer da sua
+                            casa o que vai mudar a sua vida! Você já pode
+                            começar hoje fazendo sites de casa e ganhar mais de
+                            R$10mil por mês apartir dos próximos 30 dias.
+                        </h5>
+                        <button>
+                            <p>QUERO MUDAR DE VIDA</p>
+                        </button>
+                        <div className={styles.discount}>
+                            <img src="/discountTag.svg" />
+                            <p>Apenas R$ 16,99.</p>
+                        </div>
+                    </div>
+                    <div className={styles.rightside}>
+                        <div className={styles.backgroundImages}>
+                            <img src="/pix1.svg" alt="Background 1" />
+                            <img src="/pix2.svg" alt="Background 2" />
+                            <img src="/pix3.svg" alt="Background 3" />
+                            <img src="/pix4.svg" alt="Background 4" />
+                        </div>
+                        <img
+                            src="/herbertOnly.svg"
+                            alt="Herbert"
+                            className={styles.herbert}
+                        />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default SectionOne;
--- src/components/SectionTwo/index.tsx
+++ src/components/SectionTwo/index.tsx
@@ -1,88 +1,92 @@
-import styles from "./style.module.scss";
+import styles from './style.module.scss';
 
 const SectionTwo = () => {
-  return (
-    <>
-      <main className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.top}>
-            <h2>
-              A diferença absurda entre o que você está <span>prestes a</span>
-            </h2>
-            <h2>
-              <span>adiquirir com o Rei</span> e do que está fugindo ...
-            </h2>
-          </div>
-          <div className={styles.bottom}>
-            <div className={styles.leftbox}>
-              <div className={styles.title}>
-                <h5>Curso meio boca</h5>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Faturamento incerto</p>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Professor que não vive o que ensina</p>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Falta de organização</p>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Curso básico</p>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Não ensina sobre o mercado real</p>
-              </div>
-              <div className={styles.cons}>
-                <img src="/redX.svg" />
-                <p>Sem suporte</p>
-              </div>
-            </div>
-            <div className={styles.rightbox}>
-              <div className={styles.title}>
-                <h5>
-                  Curso do <span>REI</span>
-                </h5>
-              </div>
-              <div className={styles.pros}>
-                <img src="correct.svg" />
-                <p>Faturamento alto</p>
-              </div>
-              <div className={styles.pros}>
-                <img src="correct.svg" />
-                <p>Professor vive o que ensina</p>
-              </div>
-              <div className={styles.pros}>
-                <img src="correct.svg" />
-                <p>Curso ponta a ponta</p>
-              </div>
-              <div className={styles.pros}>
-                <img src="correct.svg" />
-                <p>Bônus sobre mercado de trabalho</p>
-              </div>
-              <div className={styles.pros}>
-                <img src="correct.svg" />
-                <p>Suporte exclusivo do rei</p>
-              </div>
-              <div className={styles.logo}>
-                <img src="/logo.svg" />
-              </div>
-            </div>
-          </div>
-          <div className={styles.button}>
-            <button>
-              <p>QUERO APRENDER COM O REI</p>
-            </button>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.top}>
+                        <h2>
+                            A diferença absurda entre o que você está{' '}
+                            <span>prestes a </span>
+                            <span>adiquirir com o Rei</span> e do que está
+                            fugindo ...
+                        </h2>
+                    </div>
+                    <div className={styles.bottom}>
+                        <div className={styles.leftbox}>
+                            <div className={styles.title}>
+                                <h5>Curso meio boca</h5>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Faturamento incerto</p>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Professor que não vive o que ensina</p>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Falta de organização</p>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Curso básico</p>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Não ensina sobre o mercado real</p>
+                            </div>
+                            <div className={styles.cons}>
+                                <img src="/redX.svg" />
+                                <p>Sem suporte</p>
+                            </div>
+                        </div>
+                        <div className={styles.rightbox}>
+                            <div className={styles.title}>
+                                <h5>
+                                    Curso do <span>REI</span>
+                                </h5>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Faturamento alto</p>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Professor vive o que ensina</p>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Curso ponta a ponta</p>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Bônus sobre mercado de trabalho</p>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Dicas exclusivas</p>
+                            </div>
+                            <div className={styles.pros}>
+                                <img src="correct.svg" />
+                                <p>Suporte exclusivo do rei</p>
+                            </div>
+                            <div className={styles.logo}>
+                                <img src="/logo.svg" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.button}>
+                        <button>
+                            <p>QUERO APRENDER COM O REI</p>
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default SectionTwo;
