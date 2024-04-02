c66fbe5a254e5bb970a22606f352b76e9315dff0
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-02 10:34:09 -0300

    change type of class

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -4,7 +4,9 @@ import styles from './styles.module.scss';
 
 const Parallax = () => {
     const container = useRef(null);
-    const [isEnd, setIsEnd] = useState(false); // State to track if scroll is at the end
+    const [isEnd, setIsEnd] = useState(false);
+    // Adicionando um novo estado para controlar a aplicação da classe startClass.
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const { scrollYProgress } = useScroll({
         target: container,
@@ -16,33 +18,43 @@ const Parallax = () => {
     useEffect(() => {
         const unsubscribe = scrollYProgress.onChange((v) => {
             const progress = parseFloat(v.toFixed(2));
+            // Ativa isAnimating quando o scroll começa.
+            if (progress > 0 && progress < 1) {
+                setIsAnimating(true);
+            }
             if (progress === 1) {
-                setIsEnd(true); // Update state when scroll reaches the end
+                setIsEnd(true);
             } else {
-                setIsEnd(false); // Reset state if not at the end
+                setIsEnd(false);
+            }
+            // Desativa isAnimating quando o scroll retorna ao início ou chega ao fim.
+            if (progress === 0 || progress === 1) {
+                setIsAnimating(false);
             }
         });
 
         return () => unsubscribe();
     }, [scrollYProgress]);
 
-    // Define animation for background transition
     const containerBackgroundAnimation = {
         backgroundColor: isEnd ? '#fff' : 'initial',
     };
 
     return (
-        <motion.div // Use motion.div for the outer container to apply the animation
+        <motion.div
             ref={container}
             className={`${styles.container} ${
                 isEnd ? styles.endContainer : ''
             }`}
-            animate={containerBackgroundAnimation} // Apply the background animation based on isEnd state
+            animate={containerBackgroundAnimation}
         >
             <div className={styles.sticky}>
                 <motion.div
                     style={{ scale }}
-                    className={`${styles.el} ${isEnd ? styles.endClass : ''}`}
+                    // Aqui é onde a lógica para aplicar a classe startClass é implementada.
+                    className={`${styles.el} ${
+                        isAnimating ? styles.startClass : ''
+                    } ${isEnd ? styles.endClass : ''}`}
                 >
                     <div className={styles.imageContainer}>
                         <p>O QUE VOCE IRA APRENDER</p>
