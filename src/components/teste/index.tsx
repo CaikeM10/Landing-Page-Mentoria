6ac8de9463811f4d0867586da80770252d22451f
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-04-15 10:41:29 -0300

    landing page

--- src/components/teste/index.tsx
+++ src/components/teste/index.tsx
@@ -1,15 +1,39 @@
+import { motion, useViewportScroll } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 import styles from './styles.module.scss';
 
 const Teste = () => {
+    const { scrollYProgress } = useViewportScroll();
+    const [isSpecificPointReached, setIsSpecificPointReached] = useState(false);
+
+    useEffect(() => {
+        const checkScroll = () => {
+            const progress = scrollYProgress.get();
+
+            if (progress >= 0.99) {
+                setIsSpecificPointReached(true);
+            } else {
+                setIsSpecificPointReached(false);
+            }
+        };
+
+        const unsubscribe = scrollYProgress.onChange(checkScroll);
+
+        return () => unsubscribe();
+    }, [scrollYProgress]);
+
+    const backgroundColor = isSpecificPointReached ? '#010425' : 'white';
+    const color = isSpecificPointReached ? 'white' : 'black';
+
     return (
-        <>
-            <div className={styles.container}>
-                <ScrollAnimation animateIn="fadeIn">
-                    <h1 className={styles.teste}>TESTE</h1>
-                </ScrollAnimation>
-            </div>
-        </>
+        <motion.div className={styles.container} style={{ backgroundColor }}>
+            <ScrollAnimation animateIn="fadeIn">
+                <h1 className={styles.teste} style={{ color }}>
+                    TESTE
+                </h1>
+            </ScrollAnimation>
+        </motion.div>
     );
 };
 
