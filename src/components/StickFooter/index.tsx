e8d266715790328ae97b236c2ff5b5aba1fee154
Leonardo <leolimadorea@gmail.com>
2024-04-19 09:11:45 -0300

    fix: footer and transition colors

--- src/components/StickFooter/index.tsx
+++ src/components/StickFooter/index.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 const StickFooter = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 400) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        visibility: isVisible ? "visible" : "hidden",
+      }}
+    >
       <div className={styles.content}>
         <div className={styles.logo}>
           <img src="/logo2.svg" />
