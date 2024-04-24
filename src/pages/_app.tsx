67c03da2cbaf59ce73c17b4cf80bc438d65b5e9d
Leonardo <leolimadorea@gmail.com>
2024-04-24 11:17:38 -0300

    feat: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <link rel="icon" href="/favicon.ico" sizes="any" />;
+    </>
+  );
 }
