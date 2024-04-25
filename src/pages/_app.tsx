e7e125fccfae9e122a130b3fc0fb851e6ed64d38
Diogo Dieger <diogaodieger@gmail.com>
2024-04-24 21:57:23 -0300

    fix: pixel instalation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,39 @@ import '@/styles/globals.scss';
 import 'animate.css/animate.compat.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import ReactPixel from 'react-facebook-pixel';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const advancedMatching = {
-    em: 'some@email.com',
-
-    fn: '',
-    ln: '',
-    ph: '',
-    ge: '',
-    db: '',
-    ct: '',
-    st: '',
-    zp: '',
-    country: '',
-  };
-
-  const options = {
-    autoConfig: true,
-    debug: false,
-  };
-
-  ReactPixel.init('750336220578249', advancedMatching, options);
-
-  ReactPixel.pageView();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('react-facebook-pixel')
+        .then((ReactPixel) => {
+          ReactPixel.init(
+            '750336220578249',
+            {
+              em: 'some@email.com',
+              fn: 'FirstName', // First name
+              ln: 'LastName', // Last name
+              ph: 'PhoneNumber', // Phone number
+              ge: 'Gender', // Gender
+              db: 'DateOfBirth', // Date of birth
+              ct: 'City', // City
+              st: 'State', // State
+              zp: 'ZipCode', // Zip code
+              country: 'Country', // Country
+            },
+            {
+              autoConfig: true,
+              debug: false,
+            }
+          );
+          ReactPixel.pageView();
+        })
+        .catch((err) =>
+          console.error('Failed to load React Facebook Pixel', err)
+        );
+    }
+  }, []);
 
   return (
     <>
@@ -40,12 +48,10 @@ export default function App({ Component, pageProps }: AppProps) {
           name="og:description"
           content="Descubra o mundo da tecnologia e desenvolvimento web com o Rei dos Sites. Aprenda a criar sites do zero e transforme suas ideias em realidade."
         />
-
         <meta
           property="og:image"
           content="https://firebasestorage.googleapis.com/v0/b/nextgain-37481.appspot.com/o/Screenshot%202024-04-01%20at%2022.58.47.png?alt=media&token=c87429da-d568-4acc-9b39-e4cb579bb9ac"
         />
-
         <title>Do Zero aos Sites</title>
       </Head>
       <Component {...pageProps} />
