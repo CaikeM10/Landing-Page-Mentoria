a986fdc2e16754fdd898de094c48718c8ee55dde
DiogoDieger <diogaodieger@gmail.com>
2024-08-19 13:57:22 -0300

    tiktok route

--- src/pages/tiktok.tsx
+++ src/pages/tiktok.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-  const queryParams = "/instagram";
+  const queryParams = "/tiktok";
   console.log(queryParams);
   return (
     <>
