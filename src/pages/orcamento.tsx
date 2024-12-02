945df2f9fb61a05ebe220ef6206bfd00959c7808
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-12-02 18:57:05 -0300

    finished about part

--- src/pages/orcamento.tsx
+++ src/pages/orcamento.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/Budget/About';
 import Banner from '@/components/Budget/Banner';
 import Footer from '@/components/Budget/Footer';
 import styles from '@/styles/orcamento.module.scss';
@@ -6,6 +7,7 @@ export default function Desafio() {
     return (
         <section className={styles.container}>
             <Banner></Banner>
+            <About></About>
             <Footer></Footer>
         </section>
     );
