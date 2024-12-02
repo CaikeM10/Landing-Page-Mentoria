6cb742edab8c809223de4f6dc45cdf6d49de3b80
Leonardo Lima Dorea <leolimadorea@gmail.com>
2024-12-02 15:22:59 -0300

    fixing banner

--- src/components/Orcamento/Banner/index.tsx
+++ src/components/Orcamento/Banner/index.tsx
@@ -1,21 +1,24 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 const Banner = () => {
-    return (
-        <>
-            <section className={styles.container}>
-                <div className={styles.titleBanner}>
-                    <h1>
-                        Crie seu site ideal e veja seu negócio crescer online.
-                    </h1>
-                    <p>
-                        Um site profissional, rápido e otimizado para atrair
-                        mais clientes e aumentar suas vendas.
-                    </p>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.titleBanner}>
+            <button>• Orçamento Grátis</button>
+            <h1>Crie seu site ideal e veja seu negócio crescer online.</h1>
+            <img src="/line.svg" alt="line" />
+            <p>
+              Um site profissional, rápido e otimizado para atrair mais clientes
+              e aumentar suas vendas.
+            </p>
+            <button className={styles.button}>FAZER ORÇAMENTO GRATUITO</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Banner;
