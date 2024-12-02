d4162d1dac9ec8265f6c5e804d3409d82f45f913
Leonardo Lima Dorea <leolimadorea@gmail.com>
2024-12-02 16:07:57 -0300

    banner

--- src/components/Orcamento/Banner/index.tsx
+++ src/components/Orcamento/Banner/index.tsx
@@ -4,11 +4,12 @@ const Banner = () => {
   return (
     <>
       <section className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.titleBanner}>
-            <button>• Orçamento Grátis</button>
+        <div className={styles.titleBanner}>
+          <div className={styles.gridBackground}>
+            <button className={styles.orcamento}>• Orçamento Grátis</button>
             <h1>Crie seu site ideal e veja seu negócio crescer online.</h1>
             <img src="/line.svg" alt="line" />
+
             <p>
               Um site profissional, rápido e otimizado para atrair mais clientes
               e aumentar suas vendas.
