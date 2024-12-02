eb0627acf47acef7e7a561ebea43823bc3ef20f0
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-12-02 17:50:25 -0300

    footer maded

--- src/pages/orcamento.tsx
+++ src/pages/orcamento.tsx
@@ -1,10 +1,12 @@
 import Banner from '@/components/Orcamento/Banner';
+import Footer from '@/components/Orcamento/Footer';
 import styles from '@/styles/orcamento.module.scss';
 
 export default function Desafio() {
     return (
         <section className={styles.container}>
             <Banner></Banner>
+            <Footer></Footer>
         </section>
     );
 }
