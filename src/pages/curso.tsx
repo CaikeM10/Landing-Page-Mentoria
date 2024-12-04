e702af62c68c682570babf6bbfe53a7f669bda73
Leonardo Lima Dorea <leolimadorea@gmail.com>
2024-12-04 14:59:15 -0300

    changes

--- src/pages/curso.tsx
+++ src/pages/curso.tsx
@@ -1,40 +1,40 @@
-import Footer from '@/components/Portuguese/Footer';
-import Guarantee from '@/components/Portuguese/Guarantee';
-import Header from '@/components/Portuguese/Header';
-import Modulos from '@/components/Portuguese/Modulos';
-import MovingBars from '@/components/Portuguese/MovingBars';
-import Price from '@/components/Portuguese/Price';
-import Questions from '@/components/Portuguese/Questions';
-import SectionOne from '@/components/Portuguese/SectionOne';
-import SectionTwo from '@/components/Portuguese/SectionTwo';
-import StickFooter from '@/components/Portuguese/StickFooter';
-import Students from '@/components/Portuguese/Swiper';
-import ZoomEffect from '@/components/Portuguese/ZoomEffect';
+import Footer from "@/components/Portuguese/Footer";
+import Guarantee from "@/components/Portuguese/Guarantee";
+import Header from "@/components/Portuguese/Header";
+import Modulos from "@/components/Portuguese/Modulos";
+import MovingBars from "@/components/Portuguese/MovingBars";
+import Price from "@/components/Portuguese/Price";
+import Questions from "@/components/Portuguese/Questions";
+import SectionOne from "@/components/Portuguese/SectionOne";
+import SectionTwo from "@/components/Portuguese/SectionTwo";
+import StickFooter from "@/components/Portuguese/StickFooter";
+import Students from "@/components/Portuguese/Swiper";
+import ZoomEffect from "@/components/Portuguese/ZoomEffect";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function Home() {
-    useEffect(() => {
-        if (typeof window !== 'undefined' && window.ttq) {
-            window.ttq.track('ViewContent');
-        }
-    }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.ttq) {
+      window.ttq.track("ViewContent");
+    }
+  }, []);
 
-    const pageTitle = 'curso';
-    return (
-        <>
-            <Header />
-            <SectionOne title={pageTitle} />
-            <SectionTwo title={pageTitle} />
-            <MovingBars />
-            <Guarantee />
-            <Students title={pageTitle} />
-            <ZoomEffect />
-            <Modulos />
-            <Price title={pageTitle} />
-            <Questions />
-            <Footer />
-            <StickFooter title={pageTitle} />
-        </>
-    );
+  const pageTitle = "curso";
+  return (
+    <>
+      <Header />
+      <SectionOne title={pageTitle} />
+      <SectionTwo title={pageTitle} />
+      <MovingBars />
+      <Guarantee />
+      <Students title={pageTitle} />
+      <ZoomEffect />
+      <Modulos />
+      <Price title={pageTitle} />
+      <Questions />
+      <Footer />
+      <StickFooter title={pageTitle} buttonText="LIBERAR ACESSO" />
+    </>
+  );
 }
--- src/pages/orcamento.tsx
+++ src/pages/orcamento.tsx
@@ -4,17 +4,29 @@ import Footer from "@/components/Budget/Footer";
 import Services from "@/components/Budget/Services";
 import SwiperComponent from "@/components/Budget/Swiper";
 import Who from "@/components/Budget/Who";
+import StickFooter from "@/components/Portuguese/StickFooter";
 import styles from "@/styles/orcamento.module.scss";
 
 export default function Desafio() {
+  const pageTitle = "curso";
+
   return (
     <section className={styles.container}>
+      <a
+        href="https://wa.me/5541999179672"
+        className={styles.whatsappButton}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="/whats2.svg" alt="WhatsApp" />
+      </a>
       <Banner></Banner>
       <About></About>
       <Who />
       <Services />
       <SwiperComponent />
       <Footer></Footer>
+      <StickFooter title={pageTitle} buttonText="ENTRAR EM CONTATO" />
     </section>
   );
 }
